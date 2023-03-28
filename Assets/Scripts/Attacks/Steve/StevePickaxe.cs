3a002d9b7e7d998b853067c964298cfd9c891a23
sardorr02 <sardorr02@gmail.com>
2023-03-28 19:47:53 -0400

    setup files for steve bot

---
+++ Assets/Scripts/Attacks/Steve/StevePickaxe.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StevePickaxe : Action
+{
+
+    // references to pickaxes
+    public List<Pickaxe> Pickaxes;
+
+    private Animator _animator;
+
+    private void Start() {
+        _animator = transform.parent.GetComponent<Animator>();
+    }
+
+    public override void Execute()
+    {
+        _animator.SetBool("Piercing", true);
+        StartCoroutine(TogglePickaxes());
+    }
+
+    IEnumerator TogglePickaxes()
+    {
+        foreach (Pickaxe pickaxe in Pickaxes)
+        {
+            pickaxe.TogglePickaxe(true);
+        }
+        yield return new WaitForFixedUpdate();
+        foreach (Pickaxe pickaxe in Pickaxes)
+        {
+            pickaxe.TogglePickaxe(false);
+        }
+    }
+
+    public override void Unexecute()
+    {
+        _animator.SetBool("Piercing", false);
+    }
+
+}
