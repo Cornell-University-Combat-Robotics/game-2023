4745af1e6d416aa3d8467beeb2b6e3c8f28249c2
Linda Hu <llh87@cornell.edu>
2023-08-31 12:12:54 -0400

    Begin adding Lance

---
+++ Assets/Scripts/Attacks/Lance/LanceJump.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanceJump : Action
+{
+
+  public ParticleSystem Effects;
+    private BotMovement _botMovement;
+    private Animator _animator;
+    private Collider2D _florenceCollider; // to disable collisions while "jumping"
+
+    private void Start() {
+        _botMovement = transform.parent.GetComponent<BotMovement>();
+        _animator = transform.parent.GetComponent<Animator>();
+        _florenceCollider = transform.parent.GetComponent<Collider2D>();
+    }
+
+    IEnumerator Jump() {
+        _animator.SetTrigger("Jump");
+        _botMovement.AddImpactForce(_botMovement.transform.forward * 10f);
+        _botMovement.ToggleControllable(false);
+        _florenceCollider.isTrigger = true;
+        yield return new WaitForSeconds(0.4f);
+        _botMovement.ToggleControllable(true);
+        _florenceCollider.isTrigger = false;
+    yield return new WaitForSeconds(0.2f);
+    // emit the shockwave damage here
+    Effects.Play();
+    }
+
+    public override void Execute()
+    {
+        StartCoroutine(Jump());
+    }
+
+}
